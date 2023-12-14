ca0d5a0909021a812729acf92f81008189d2335c
Philip Raftheim <phra0306@storsjogymnasiet.se>
2023-12-14 11:40:38 +0100

    komplettering

--- knappen/knappen.js
+++ knappen/knappen.js
@@ -1,19 +1,24 @@
-let siffra=0
 
-function plusclicker(){
+const ljud = document.getElementById("ljud")
+const minusknapp = document.getElementById("minus")
+
+let siffra = 0
+
+function plusclicker() {
     //alert("hej")
-    siffra = siffra +1
+    siffra = siffra + 1
     console.log(siffra)
     document.getElementById("taltext").innerHTML = siffra
+    ljud.play()
 }
 
-const minusknapp = document.getElementById("minus")
-minusknapp.addEventListener("click",minusClick) 
-
-function minusClick(){
-//siffra=siffra - 1
-//siffra = - siffra
-siffra--
-document.getElementById("taltext").innerHTML = siffra
-console.log (siffra)
+function minusClick() {
+    //siffra=siffra - 1
+    //siffra = - siffra
+    siffra--
+    document.getElementById("taltext").innerHTML = siffra
+    console.log(siffra)
+    ljud.play()
 }
+
+minusknapp.addEventListener("click", minusClick)
